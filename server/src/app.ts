faf32843a7e5deae40fb8e52adee980c9b352149
Aljosa Boskovic <aljosa.boskovic@redacreltd.com>
2025-05-15 19:22:46 +0200

    Add config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Test express");
+app.get("/api", (req, res) => {
+  res.send("Root api test");
 });
+
+app.use(express.static(join(__dirname, "../../client/dist")));
 
 export default app;
