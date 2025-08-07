16370a74ee3b7368c5a44886faeb93a54cc5dc5b
Aljosa Boskovic <aljosa.boskovic@redacreltd.com>
2025-08-07 13:32:39 +0200

    Add redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { router } from "./routes.ts";
+import { router } from "./routes";
 import "./index.css";
 
 import { RouterProvider } from "react-router-dom";
+import { persistor, store } from "./store.ts";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <RouterProvider router={router} />
-
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,5 +1,13 @@
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { increment } from "@features/counter/counterSlice";
+
 export const Home = () => {
+  const count = useAppSelector((state) => state.counter?.value);
+  const dispatch = useAppDispatch();
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment counter</button>
+    </div>
+  );
+};
